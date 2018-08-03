caeeba312764c6a38c386da5b3c9f9139e4948b7
Louis Wu <louis.wu@garmin.com>
2018-08-03 08:44:53 +0800

    Add LiveChartEnhancement.Sample.

---
+++ CSharpTouchEnhancement/LiveChartEnhancement.Sample/MainWindowViewModel.cs
@@ -0,0 +1,39 @@
+﻿using LiveCharts;
+using LiveCharts.Events;
+using LiveCharts.Wpf;
+using Prism.Commands;
+using System;
+
+namespace LiveChartEnhancement.Sample
+{
+    public class MainWindowViewModel
+    {
+        public DelegateCommand<ChartPoint> DataHoverCommand { get; set; }
+        public DelegateCommand<ChartPoint> DataClickCommand { get; set; }
+        public DelegateCommand<CartesianChart> UpdaterTickCommand { get; set; }
+        public DelegateCommand<RangeChangedEventArgs> RangeChangedCommand { get; set; }
+
+        public MainWindowViewModel()
+        {
+            DataClickCommand = new DelegateCommand<ChartPoint>(p =>
+            {
+                Console.WriteLine("[COMMAND] you clicked " + p.X + ", " + p.Y);
+            });
+
+            DataHoverCommand = new DelegateCommand<ChartPoint>(p =>
+            {
+                Console.WriteLine("[COMMAND] you hovered over " + p.X + ", " + p.Y);
+            });
+
+            UpdaterTickCommand = new DelegateCommand<CartesianChart>(c =>
+            {
+                Console.WriteLine("[COMMAND] Chart was updated!");
+            });
+
+            RangeChangedCommand = new DelegateCommand<RangeChangedEventArgs>(e =>
+            {
+                Console.WriteLine("[COMMAND] Axis range changed");
+            });
+        }
+    }
+}
